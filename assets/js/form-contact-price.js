1a48be10b45d60d58b33882b7226f57f2b555f23
Dimitrios Chantzis <chantzis.dimitrios@gmail.com>
2017-08-25 01:36:53 +0100

    Updating branding + social media share + editing forms

---
+++ assets/js/form-contact-price.js
@@ -0,0 +1,83 @@
+$(window).load(function() {
+
+  var $form = $('.form-contact-price');
+
+  if($form.length > 0) {
+
+    var formValues = {};
+
+    $form.on('submit', function (e) {
+      e.preventDefault();
+
+      $('input[type="submit"]').val('Submitting...');
+      $form.addClass('form-submitting');
+
+      var validationError = false;
+      var formTitle = $form.attr('data-title');
+      var $formElements = $('.' + formTitle + ' input' + ', ' + '.' + formTitle + ' textarea' );
+      formResetErrors(formTitle);
+      formValues = {};
+
+      $formElements.each(function(i, v){
+        if (v.type !== 'submit'
+            && !$(this).hasClass("hidden")) {
+              fieldHumanName = $(this).attr('data-title').split('--').pop().replace(/\s*/g,'').replace('-', ' ');
+              fieldName = v.name.split('--').pop().replace(/\s*/g,'');
+
+            if( $(this).val().trim() ){
+              formValues[fieldName] = v.value;
+            } else {
+              validationError = true;
+
+              //if("full-name" == fieldName) {
+                $('.' + formTitle + ' fieldset.form-contact-price-full-name').addClass('form-error');
+                $('.' + formTitle + ' fieldset.form-contact-price-full-name .field-error').remove();
+                $('.' + formTitle + ' fieldset.form-contact-price-full-name').prepend('<div class="field-error push-1-4 gutter-1-2 pad-t-1-4 pad-b-1-4 milli">name is required.' + '</div>');
+              //} else {
+                $('.' + formTitle + ' fieldset.' + $(this).attr('data-title')).addClass('form-error');
+                $('.' + formTitle + ' fieldset.' + $(this).attr('data-title')).prepend('<div class="field-error push-1-4 gutter-1-2 pad-t-1-4 pad-b-1-4 milli">' + fieldHumanName + ' is required.' + '</div>');
+              //}
+
+            }
+        }
+      });
+
+      if( !validationError ) {
+        console.log(formValues);
+        $.ajax({
+          url: $form.attr('action'),
+          method: "POST",
+          data: formValues,
+          dataType: "json",
+          success: function(resp) {
+            $form.removeClass('form-submitting');
+            $('.' + formTitle + ' .form-contact-price-notification').text('Thank you for contacting me.').removeClass('hidden');
+            $('.' + formTitle + ' input, ' + '.' + formTitle + ' textarea').val('');
+            $('.' + formTitle + ' input[type="submit"]').val('Send it.');
+          },
+          error: function(resp) {
+            $('.' + formTitle + ' .form-contact-price-notification').text('There was an error submitting the form.').removeClass('hidden').addClass('form-error');
+            $form.removeClass('form-submitting');
+            $('.' + formTitle + ' input[type="submit"]').val('Send it.');
+          }
+        });
+      } else {
+        $('.' + formTitle + ' .form-contact-price-notification').text('Your form has encountered a problem. Please scroll down to review.').removeClass('hidden').addClass('form-error');
+        $form.removeClass('form-submitting');
+        $('.' + formTitle + ' input[type="submit"]').val('Send it.');
+      }
+
+
+    });
+
+  }
+
+});
+
+function formResetErrors(formTitle) {
+  $('.' + formTitle + ' .form-contact-price-notification').text('').addClass('hidden').removeClass('form-error');
+  $('.' + formTitle + ' input, '+'.' + formTitle + ' select').removeClass('input-error');
+  $('.' + formTitle + ' label.form-error').html("");
+  $('.' + formTitle + ' .field-error').remove();
+  $('.' + formTitle + ' fieldset').removeClass('form-error');
+}
