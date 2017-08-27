68fd103dc22d2e579e5a7eadcfe34f297e84d8d8
Dimitrios Chantzis <chantzis.dimitrios@gmail.com>
2017-08-27 01:02:43 +0100

    Updating social media share buttons

--- assets/js/social-media-share.js
+++ assets/js/social-media-share.js
@@ -1,13 +1,40 @@
-if($('.social-btns').length > 0) {
-    $('.social-btn').on('click', function(e){
-        e.preventDefault();
-        socialURL = $(this).attr('href');
-        //encodeURIComponent(social_url)
-        window.open(
-          socialURL,
-          '',
-          'status=1, width=550,height=420');
+$(window).load(function(){
 
-        return false;
+  if($('.social-media-share-btns').length > 0) {
+
+    $('.enable-social-btns-overlay').on('click', function() {
+      console.log('asda');
+      $(".social-btns-overlay").addClass('js-social-btns-container-fade');
     });
-}
+
+  }
+
+  if($('.social-btns').length > 0) {
+      $('.social-btn').on('click', function(e){
+          e.preventDefault();
+          socialURL = $(this).attr('href');
+          //encodeURIComponent(social_url)
+          window.open(
+            socialURL,
+            '',
+            'status=1, width=550,height=420');
+
+          return false;
+      });
+  }
+
+  if($('input.share-page-url').length > 0) {
+    $('input.share-page-url').on("click", function () {
+       $(this).select();
+    });
+  }
+
+  var enableSocialBtnsOverlay = function() {
+    $('.social-btns-overlay').removeClass('js-social-btns-container-fade');
+  }
+
+  $('.btn-close-social-btns').on('click', function(){
+    enableSocialBtnsOverlay();
+  });
+
+});
